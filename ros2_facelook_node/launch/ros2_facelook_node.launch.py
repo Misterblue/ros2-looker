4622d5de5afb253fa65262382e5ce4fa8d6007fd
Robert Adams <misterblue@misterblue.com>
2021-05-12 20:09:30 -0700

    Facelook: first version of launch.py

---
+++ ros2_facelook_node/launch/ros2_facelook_node.launch.py
@@ -0,0 +1,16 @@
+import launch
+import launch.actions
+import launch.substitutions
+import launch_ros.actions
+
+
+def generate_launch_description():
+    return launch.LaunchDescription([
+        launch.actions.DeclareLaunchArgument(
+            'node_prefix',
+            default_value=[launch.substitutions.EnvironmentVariable('USER'), '_'],
+            description='Prefix for node names'),
+        launch_ros.actions.Node(
+            package='ros2_facelook_node', executable='service', output='screen',
+            name=[launch.substitutions.LaunchConfiguration('node_prefix'), 'talker']),
+    ])
