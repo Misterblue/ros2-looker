327e2cc3ea7554f832fca2c7017994d201297a39
Robert Adams <misterblue@misterblue.com>
2021-05-18 13:51:20 -0700

    docker: add LOOKER_DEBUG environment variable to docker running script

--- docker/files/looker-start.sh
+++ docker/files/looker-start.sh
@@ -6,6 +6,9 @@ source ros2-looker/install/local_setup.bash
 PACKAGE=${LOOKER_PACKAGE}
 NODE=${LOOKER_NODE:-service}
 
+# If environment var "LOOKER_DEBUG" is set, output debug messages
+IFDEBUG=${LOOKER_DEBUG:-no}
+
 for pkg in "ros2_adafruit_pwmhat_node" "ros2_facelook_node" "ros2_facefinder_node" "ros2_raspicam_node" ; do
     if [[ "$PACKAGE" == "$pkg" ]] ; then
         OK=yes
@@ -17,6 +20,11 @@ if [[ "$OK" != "yes" ]] ; then
     exit 5
 fi
 
+DEBUGG=""
+if [[ "$IFDEBUG" != "no" ]] ; then
+    DEBUGG="--ros-args --log-level debug"
+fi
+
 echo "Running executable $NODE in package $PACKAGE"
-ros2 run "$PACKAGE" "$NODE"
+ros2 run "$PACKAGE" "$NODE" ${DEBUGG}
 
